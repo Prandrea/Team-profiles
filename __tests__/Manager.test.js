1e3d88fa6e26b149afae2537c60f490329e0e326
prandrea <prandrea@utexas.edu>
2022-06-09 17:51:10 -0500

    all tests passed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,8 +3,8 @@ const Manager = require('../lib/Manager');
 
 
 test('Creates a Manager office number', () => {
-    var employee = new Manager('Andrea', 10, 'test@email.com', 'testOffice');
+    var employee = new Manager('Andrea', 10, 'test@email.com', 1);
 
-    expect(employee.office).toBe('testOffice');
+    expect(employee.officeNum).toBe(1);
     
 });
